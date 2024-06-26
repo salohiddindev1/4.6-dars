166b4cfc77e3976f01f76f0dc582cf929bc78e79
salohiddindev1 <169024491+salohiddindev1@users.noreply.github.com>
2024-06-26 11:48:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+
+let son1 
+let son2 
+let son3 
+let son4 
+
+// ifwjofjwodj
+
+let num1 = +prompt("Birinchi sonni kiriting: ")
+let num2 = +prompt("Ikkinchi sonni kiriting: ")
+ 
+ let amal =  prompt("Amalni tanlang: \n" +
+     "1.Qo'shish. \n"  +
+      "2.Ayirish.  \n" + 
+    "3.Ko'paytirish. \n" +
+"4.Bo'lish." +
+ "Amalni tanlang(1/2/3/4): ")
+
+
+//  shart
+
+if(amal == 1){
+ alert(`${num1} + ${num2} = ${num1 + num2}`)
+} else if (amal == 2){
+ alert(`${num1} - ${num2} = ${num1 - num2}`)
+} else if (amal == 3){
+ alert(`${num1} * ${num2} = ${num1 * num2}`)
+} else if (amal == 4){
+    alert(`${num1} / ${num2} = ${num1 / num2}`)
+} 
+ 
+
+
